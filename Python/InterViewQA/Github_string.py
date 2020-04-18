821d40a121fb14a67cff755f28aad16a019a4076
muyuchenzi <muyuchenzi@gmail.com>
2020-04-18 11:42:19 +0800

    test-git push test-none

--- Python/InterViewQA/Github_string.py
+++ Python/InterViewQA/Github_string.py
@@ -12,7 +12,7 @@ def string_demo():
     string_var.replace(r"\d", "[number]")
     numb = re.findall(r'\d{2}', string_var)
     sss = re.sub(r'\d', "[number]", string=string_var)
-
+    #
     # re.match() 从头开始查找一个pattern，然后返回类似于与StartWith，好像没有什么特别的用处 返回一个Match Object
     ss = re.match('123', '123djaffd123')
     # re.search() 从头开始 返回一个Match Object
